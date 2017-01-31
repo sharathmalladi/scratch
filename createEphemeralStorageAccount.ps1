c98deee058f39800ffb10d468bf34cbbcc489d3f
smalladi78 <msharath@live.com>
2017-01-30 20:07:07 -0800

    Updated to create and output sasToken

--- createEphemeralStorageAccount.ps1
+++ createEphemeralStorageAccount.ps1
@@ -1,42 +1,65 @@
-try
+Workflow CreateEphemeralStorageAccount
 {
-    $servicePrincipalConnection = Get-AutomationConnection -Name 'AzureRunAsConnection'
 
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+   Param
+    (
+        [OutputType([string])]
+        $storageOutput
 
-    Write-Output "Successfully logged in to Azure." 
+    )
+    try
+    {
+        $connectionName = 'AzureRunAsConnection'
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+        Write-Output "Successfully logged in to Azure." 
+    }
+    catch
+    {
+        if (!$servicePrincipalConnection) 
+        { 
+            $ErrorMessage = "Connection $connectionName not found." 
+            throw $ErrorMessage 
+        }  
+        else 
+        { 
+            Write-Error -Message $_.Exception 
+            throw $_.Exception 
+        } 
+    }
+
+    Select-AzureRmSubscription -SubscriptionId $servicePrincipalConnection.SubscriptionID
+
+    $location = Get-AutomationVariable -Name 'location'
+    $resourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
+
+    $storageAccountName = 'datadrop{0:yyyyMMddhhmmss}' -f [System.DateTime]::UtcNow
+    $containerName = 'data'
+
+    $storageAccount = New-AzureRmStorageAccount `
+        -AccessTier Hot `
+        -Kind BlobStorage `
+        -Location $location `
+        -Name $storageAccountName `
+        -ResourceGroupName $resourceGroupName `
+        -SkuName Standard_LRS
+
+    $key = (Get-AzureRmStorageAccountKey -Name $storageAccountName -ResourceGroupName $resourceGroupName).Value[0]
+    $context = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key
+    $container = New-AzureStorageContainer -Name $containerName -Context $context
+    $expiryDate = (get-date).AddDays(2)
+    $sasToken = New-AzureStorageContainerSASToken -Name $containerName -Context $context -ExpiryTime $expiryDate -FullUri
+
+    $storageOutput = @{
+    storageAccountName = $storageAccountName
+    sasToken = $sasToken
+    }
+
+    Write-Output $storageOutput
+
 }
-catch
-{
-    if (!$servicePrincipalConnection) 
-    { 
-        $ErrorMessage = "Connection $connectionName not found." 
-        throw $ErrorMessage 
-    }  
-    else 
-    { 
-        Write-Error -Message $_.Exception 
-        throw $_.Exception 
-    } 
-}
-
-Select-AzureRmSubscription -SubscriptionId $servicePrincipalConnection.SubscriptionID
-
-$location = Get-AutomationVariable -Name 'location'
-$resourceGroupName = Get-AutomationVariable -Name 'resourceGroupName'
-
-$storageAccountName = 'datadrop{0:yyyyMMddhhmmss}' -f [System.DateTime]::UtcNow
-
-$storageAccount = New-AzureRmStorageAccount `
-    -AccessTier Hot `
-    -Kind BlobStorage `
-    -Location $location `
-    -Name $storageAccountName `
-    -ResourceGroupName $resourceGroupName `
-    -SkuName Standard_LRS
-
-Write-Output -InputObject $storageAccount.Name
